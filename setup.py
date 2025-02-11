db82261ee7de39f9517107dbc127c787e2778d6c
badgkat <109937927+badgkat@users.noreply.github.com>
2025-02-11 09:44:24 -0700

    fix PyPI uploading

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="chess-builder",
-    version="0.1.0",
+    name="chessbuilder",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "pygame>=2.6.1",
